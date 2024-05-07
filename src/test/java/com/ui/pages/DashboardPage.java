3ff0178deddb9697678d70185996b776997039a5
Bhagyashree Sahu <bhagyashree25.sahu@gmail.com>
2024-05-07 16:38:08 +0400

    Update DashboardPage.java

--- src/test/java/com/ui/pages/DashboardPage.java
+++ src/test/java/com/ui/pages/DashboardPage.java
@@ -12,6 +12,8 @@ public final class DashboardPage extends BrowserUtil {
 	private static final By SEARCH_TEXTBOX_LOCATOR = By.xpath("//input[@name='field-keywords']");
 	private static final By SEARCH_ICON_LOCATOR = By.xpath("//input[@value='Go']");
 	private static final By SEARCHRESULT_TEXT_LOCATOR = By.xpath("//span[contains(text(),'results for')]/../span[3]");
+	private static final By SEARCHRESULTINFO_BAR_LOCATOR = By.xpath("//span[contains(text(),'results for')]/../span[1]");
+	private static final By SEARCHRESULT_ROW_LOCATOR = By.xpath("//div[@data-component-type='s-search-result']");
 
 
 	private WebDriver wd;
@@ -31,6 +33,9 @@ public final class DashboardPage extends BrowserUtil {
 		enterText(SEARCH_TEXTBOX_LOCATOR,sKeyword );
 		clickOn(SEARCH_ICON_LOCATOR);
 		String searchResultFound = getVisibleText(SEARCHRESULT_TEXT_LOCATOR).replace("\"", "");
+		String SearchResultInfo = getVisibleText(SEARCHRESULTINFO_BAR_LOCATOR);
+		System.out.println(SearchResultInfo + " " + sKeyword);
+		searchResult(SEARCHRESULT_ROW_LOCATOR);
 		return searchResultFound;
 	}
 
